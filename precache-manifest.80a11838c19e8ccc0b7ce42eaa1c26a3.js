95feecac873793e6b2ee8882961a58f5b761b26d
Carson Johnson <carson74johnson@gmail.com>
2021-12-07 15:32:39 -0500

    Updates

---
+++ precache-manifest.80a11838c19e8ccc0b7ce42eaa1c26a3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeb99c77d431d03d95d6c6961598d06e",
+    "url": "/Updated-React-Portfolio/index.html"
+  },
+  {
+    "revision": "4919fc0ce0076f737f2c",
+    "url": "/Updated-React-Portfolio/static/css/2.451bf2e4.chunk.css"
+  },
+  {
+    "revision": "a57d6d28de82e77a22fb",
+    "url": "/Updated-React-Portfolio/static/css/main.2656ca6f.chunk.css"
+  },
+  {
+    "revision": "4919fc0ce0076f737f2c",
+    "url": "/Updated-React-Portfolio/static/js/2.5b443a1e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Updated-React-Portfolio/static/js/2.5b443a1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a57d6d28de82e77a22fb",
+    "url": "/Updated-React-Portfolio/static/js/main.3166cb55.chunk.js"
+  },
+  {
+    "revision": "c9eb194ada00bf6131ed",
+    "url": "/Updated-React-Portfolio/static/js/runtime-main.8ed5829a.js"
+  },
+  {
+    "revision": "7cfc12b2fa69950eb60e6053b2c85d6e",
+    "url": "/Updated-React-Portfolio/static/media/CLI Team Profile Generator.7cfc12b2.jpg"
+  },
+  {
+    "revision": "6c0d8a604029ae701e6db85303687e89",
+    "url": "/Updated-React-Portfolio/static/media/Cooking Crafty.6c0d8a60.jpg"
+  },
+  {
+    "revision": "67f7561fba55ff7e940e5338a05aa498",
+    "url": "/Updated-React-Portfolio/static/media/Fitness Tracker.67f7561f.jpg"
+  },
+  {
+    "revision": "cbad1c9399661284472e24164670fa83",
+    "url": "/Updated-React-Portfolio/static/media/Geo Stream.cbad1c93.jpg"
+  },
+  {
+    "revision": "456bf91793d2bf90a317692a26d0e7c9",
+    "url": "/Updated-React-Portfolio/static/media/MVC Tech Blog.456bf917.jpg"
+  },
+  {
+    "revision": "474a7d109ee3dd21207147300a97c3dc",
+    "url": "/Updated-React-Portfolio/static/media/Note Taker App.474a7d10.jpg"
+  },
+  {
+    "revision": "90b3af0c2a6f50685020cc803f323ee1",
+    "url": "/Updated-React-Portfolio/static/media/cover-image.90b3af0c.jpg"
+  },
+  {
+    "revision": "e620d356e5c76a3bc0ab3cc8f636802f",
+    "url": "/Updated-React-Portfolio/static/media/profile-image.e620d356.png"
+  }
+]);
